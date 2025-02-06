8b02e25da8e2efa54bcd71b0b14b23a18773bd0d
Ravi Raj <99949881+Ravi-rprithvi@users.noreply.github.com>
2025-02-06 14:54:55 +0530

    Create dockerBuildAndPush.groovy

---
+++ dockerBuildAndPush.groovy
@@ -0,0 +1,7 @@
+def call(String imageName, String tag) {
+    echo "Building Docker image ${imageName}:${tag}"
+    sh """
+        docker build -t ${imageName}:${tag} .
+        docker push ${imageName}:${tag}
+    """
+}
